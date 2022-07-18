7c3730613cbb148f4f09204e3a2a90d503b9ed7b
Lexih <67009568+xLexih@users.noreply.github.com>
2022-07-18 17:31:48 +0200

    Donno having fun
    + more story to the text
    + special timings for yk FUN

--- html.js
+++ html.js
@@ -1,14 +1,37 @@
 
 document.addEventListener('DOMContentLoaded',function(event){
 
-var dataText = [ "why are you here smh smh", "How cute, you think i can program?", "Guess what...", "mostly stolen! hehehe"];
+var time = 125
+var dataText = [
+"why are you here smh smh",
+"How cute, you think i can program?",
+"Guess what...",
+"mostly stolen! heheHEHEHEHEHEHEHE",
+"anyway you probably didnt notice i spelt \"Programming\" wrong <3"];
 
 function typeWriter(text, i, fnCallback) {
 	if (i < (text.length)) {
 		document.getElementById("TEXT").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+		if (text.includes("mostly stolen!"))
+		switch(i){
+			case 13:
+				time = 850;
+				break;
+			case i>13:
+				time = 50;
+				break;
+			case i>20:
+				time = 10;
+				break;
+			case i>20:
+				time = 10;
+				break;
+			default:
+				time = 125;
+		}
 		setTimeout(function() {
 			typeWriter(text, i + 1, fnCallback)
-		}, 125);
+		}, time);
 	}
 	else if (typeof fnCallback == 'function') {	
 		setTimeout(fnCallback, 700);
