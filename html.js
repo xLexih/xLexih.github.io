59d19a4fbc4c08b86aa594bb72574a5d013bbc39
Lexih <67009568+xLexih@users.noreply.github.com>
2022-07-18 17:17:00 +0200

    Changes
    * Changed up the animation JS
    + Fixed the bug given by undefined

--- html.js
+++ html.js
@@ -1,45 +1,31 @@
 
 document.addEventListener('DOMContentLoaded',function(event){
-// array with texts to type in typewriter
+
 var dataText = [ "why are you here smh smh", "How cute, you think i can program?", "Guess what...", "mostly stolen! hehehe"];
 
-// type one text in the typwriter
-// keeps calling itself until the text is finished
 function typeWriter(text, i, fnCallback) {
-	// chekc if text isn't finished yet
-		if (i < (text.length)) {
-	// add next character to h1
-	
-	document.getElementById("TEXT").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
-
-	// wait for a while and call this function again for next character
-	setTimeout(function() {
-		typeWriter(text, i + 1, fnCallback)
-	}, 125);
+	if (i < (text.length)) {
+		document.getElementById("TEXT").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+		setTimeout(function() {
+			typeWriter(text, i + 1, fnCallback)
+		}, 125);
 	}
-	// text finished, call callback if there is a callback function
 	else if (typeof fnCallback == 'function') {	
-	// call callback after timeout
-	setTimeout(fnCallback, 700);
+		setTimeout(fnCallback, 700);
 	}
 }
-// start a typewriter animation for a text in the dataText array
 function StartTextAnimation(i) {
-	if (typeof dataText[i] == 'undefined'){
+	if (typeof dataText[i] === 'undefined'){
 		setTimeout(function() {
-		StartTextAnimation(0);
+			StartTextAnimation(0);
 		}, 3000);
 	}
-	// check if dataText[i] exists
-	if (i < dataText[i].length) {
-	// text exists! start typewriter animation
-	typeWriter(dataText[i], 0, function(){
-	// after callback (and whole text has been animated), start next text
-	StartTextAnimation(i + 1);
-	});
+	else if (i < dataText[i].length) {
+		typeWriter(dataText[i], 0, function(){
+			StartTextAnimation(i + 1);
+		});
 	}
 }
 
-// start the text animation
 StartTextAnimation(0);
 });
